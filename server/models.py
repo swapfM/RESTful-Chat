ea49add85690cd92a36e0d60ddbb08bd2e54aee4
swapfm <singhswapnil507@gmail.com>
2023-12-27 13:05:38 +0530

    made views

--- server/models.py
+++ server/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+
+
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+
+    # Delete existing category icon if updating the category
+    def save(self, *args, **kwargs):
+        if self.id:
+            existingCategory = get_object_or_404(Category, id=self.id)
+            if existingCategory.icon != self.icon:
+                existingCategory.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    # Delete the icon if the category is deleted
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
--- server/views.py
+++ server/views.py
@@ -5,12 +5,14 @@ from .serializer import ServerSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.db.models import Count
+from .schema import server_list_docs
 
 
 class ServerListViewSet(viewsets.ViewSet):
     # Initial queryset containing all servers
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         # Extracting query parameters
         category = request.query_params.get("category")
@@ -20,21 +22,21 @@ class ServerListViewSet(viewsets.ViewSet):
         with_num_members = request.query_params.get("with_num_members") == "true"
 
         # Check authentication if filtering by user
-        if by_user and not request.user.is_authenticated:
+        if not request.user.is_authenticated:
             raise AuthenticationFailed()
 
         # Filtering by category if provided
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        # Annotating queryset with the number of members if requested
-        if with_num_members:
-            self.queryset = self.queryset.annotate(num_members=Count("members"))
-
         # Filtering by the current user if requested
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(members=user_id)
+
+        # Annotating queryset with the number of members if requested
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("members"))
 
         # Limiting the queryset by quantity if provided
         if qty:
